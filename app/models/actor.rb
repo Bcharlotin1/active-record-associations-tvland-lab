0feb23ae9d8542fc2637d9494fee4f4c0a18e39d
bcharlotin1 <biancacharlotin1@gmail.com>
2021-02-26 18:37:11 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters 
+
+    def full_name
+        first = self.first_name
+        last = self.last_name
+        fullname = "#{first} #{last}"
+    end
+
+    def list_roles
+ 
+        array =[]
+        self.characters.each do |obj|
+            array << "#{obj.name} - #{obj.show.name}"
+        end 
+        array
+    end
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
+require 'pry'
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actor_list
+ 
+        array =[]
+        self.characters.each do |obj|
+
+            # binding.pry
+        end 
+        array
+    end
 end
